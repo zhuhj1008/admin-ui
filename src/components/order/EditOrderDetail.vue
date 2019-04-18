85ff51e2e9f3717f0be413b5c693e2ed1765737f
zhuhongjun <zhuhongjun123>
2019-04-18 18:10:13 +0800

    nothing

--- src/components/order/EditOrderDetail.vue
+++ src/components/order/EditOrderDetail.vue
@@ -2,40 +2,75 @@
 	<div>
 		<el-button @click="dialogFormVisible = true" type="text" icon="el-icon-plus" size="small"></el-button>
 		<el-dialog title="添加订单" :visible.sync="dialogFormVisible" width='80%' append-to-body>
-		<el-button type="primary" icon="el-icon-plus" @click="addUser()" size='mini'>添加</el-button>
+			<el-button type="primary" icon="el-icon-plus" @click="addUser()" size='mini'>添加</el-button>
+			
+			<!-- 已经保存的 -->
+			<template v-for="orderDetail in orderDetails">
+			<el-form :inline="true" :model="order" class="demo-form-inline" size='mini' disabled>
+				
+					<div>
+						<el-form-item>
+							<el-input v-model="orderDetail.length" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-input v-model="orderDetail.width" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-input v-model="orderDetail.hight" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-input v-model="orderDetail.hight" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-input v-model="orderDetail.length" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-input v-model="orderDetail.hight" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-input v-model="orderDetail.length" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item>
+							<el-input v-model="orderDetail.length" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						
+					</div>
+					</el-form>
+					<el-button @click="" size="mini">修改</el-button>
+				</template>
 			<el-form :inline="true" :model="order" class="demo-form-inline" size='mini'>
-			  <!-- 动态生成 -->
-			  <div class="moreRules">
-				  <!-- :rules="[{required: true, message: '请完善信息'}]" -->
-				  <div class="moreRulesIn" v-for="(item, index) in order.orderDetails" :key="item.key" >
-					  <el-form-item  label="" :prop="'orderDetails.' + index +'.length'" >
-					  <el-input v-model="item.length" placeholder="长"  class="el-select_box input-order"></el-input>
-					  </el-form-item>
-					  <el-form-item  label="" :prop="'orderDetails.'+ index +'.width'" >
-					  <el-input v-model="item.width" placeholder="宽"  class="el-select_box input-order"></el-input>
-					  </el-form-item>
-					  <el-form-item  label="" :prop="'orderDetails.'+ index +'.hight'" >
-					  <el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
-					  </el-form-item>
-					  <el-form-item  label="" :prop="'orderDetails.'+ index +'.hight'" >
-					  <el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
-					  </el-form-item>
-					  <el-form-item  label="" :prop="'orderDetails.'+ index +'.hight'" >
-					  <el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
-					  </el-form-item>
-					  <el-form-item  label="" :prop="'orderDetails.'+ index +'.hight'" >
-					  <el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
-					  </el-form-item>
-					  <el-form-item  label="" :prop="'orderDetails.'+ index +'.hight'" >
-					  <el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
-					  </el-form-item>
-					  <el-form-item  label="" :prop="'orderDetails.'+ index +'.hight'" >
-					  <el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
-					  </el-form-item>
-					  <el-button @click="deleteRules(item, index)" size="mini">删除</el-button>
-				  </div>
-			  </div>
-			  </el-form>
+				<!-- 动态生成的 -->
+				<div class="moreRules">
+					<!-- :rules="[{required: true, message: '请完善信息'}]" -->
+					<div class="moreRulesIn" v-for="(item, index) in order.addOrderDetails" :key="item.key" >
+						<el-form-item  label="" :prop="'addOrderDetails.' + index +'.length'" >
+						<el-input v-model="item.length" placeholder="长"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item  label="" :prop="'addOrderDetails.'+ index +'.width'" >
+						<el-input v-model="item.width" placeholder="宽"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item  label="" :prop="'addOrderDetails.'+ index +'.hight'" >
+						<el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item  label="" :prop="'addOrderDetails.'+ index +'.hight'" >
+						<el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item  label="" :prop="'addOrderDetails.'+ index +'.hight'" >
+						<el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item  label="" :prop="'addOrderDetails.'+ index +'.hight'" >
+						<el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item  label="" :prop="'addOrderDetails.'+ index +'.hight'" >
+						<el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-form-item  label="" :prop="'addOrderDetails.'+ index +'.hight'" >
+						<el-input v-model="item.hight" placeholder="高"  class="el-select_box input-order"></el-input>
+						</el-form-item>
+						<el-button @click="deleteRules(item, index)" size="mini">删除</el-button>
+					</div>
+				</div>
+			</el-form>
 		</el-dialog>
 	</div>
 </template>
@@ -44,9 +79,19 @@
 	  export default {
     data() {
       return {
+				orderDetails:[
+					{
+					length:'1',
+					width:'2',
+					hight:'3'
+					},{
+					length:'1',
+					width:'2',
+					hight:'3'
+					},
+				],
 				order:{
-					
-					orderDetails:[{
+					addOrderDetails:[{
 					  length:'',
 					  width:'',
 					  hight:''
@@ -57,16 +102,16 @@
     },
 		methods: {
 		   addUser() {
-		        this.order.orderDetails.push({
+		        this.order.addOrderDetails.push({
 		            field:'',
 		            name:'',
 		            max:'',
 		      })
 		  },
 		   deleteRules(item, index) {
-		        this.index = this.order.orderDetails.indexOf(item)
+		        this.index = this.order.addOrderDetails.indexOf(item)
 		        if (index !== -1) {
-		            this.order.orderDetails.splice(index, 1)
+		            this.order.addOrderDetails.splice(index, 1)
 		        }
 		  }
 		}
