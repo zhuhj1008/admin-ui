50e9b123159ddb505fc24b00114601bc670511b5
zhuhongjun <zhuhongjun123>
2019-05-21 18:17:07 +0800

    feat 图例 新增订单80% By Joe 20190508

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,77 +1,82 @@
 <template>
-	<div class="home">
-		<el-container>
-			<el-header><admin-header></admin-header></el-header>
-			<el-main>
-					<!-- 二级路由 -->
-					<router-view></router-view>
-			</el-main>
-			<!-- 外层包一层是为了footer固定在底部 -->
-			<!--<div class="footer">-->
-				<el-footer><admin-footer></admin-footer></el-footer>
-			<!--</div>-->
-			
-		</el-container>
-	
+  <div class="home">
+    <el-container>
+      <el-header>
+        <admin-header></admin-header>
+      </el-header>
+      <el-main>
+        <!-- 二级路由 -->
+        <router-view></router-view>
+      </el-main>
+      <!-- 外层包一层是为了footer固定在底部 -->
+      <!--<div class="footer">-->
+      <el-footer>
+        <admin-footer></admin-footer>
+      </el-footer>
+      <!--</div>-->
+
+    </el-container>
+
   </div>
 </template>
 
 <script>
-import AdminHeader from '@/components/AdminHeader'
-import AdminFooter from '@/components/AdminFooter'
-export default {
-  name: 'Home',
-  data () {
-    return {
+  import AdminHeader from '@/components/AdminHeader'
+  import AdminFooter from '@/components/AdminFooter'
+
+  export default {
+    name: 'Home',
+    data() {
+      return {}
+    },
+    components: {
+      "admin-header": AdminHeader,
+      "admin-footer": AdminFooter,
     }
-  },
-  components:{
-	"admin-header":AdminHeader,
-	"admin-footer":AdminFooter,
   }
-}
 </script>
 
-	
+
 <style scoped="true">
-.footer {position:absolute;
-	bottom:0;
-	width:100%;
-	height:30px;
-	background-color: #ffcc00;
-}
-	
-.el-header{
-	background-color: #ffcc00;
-	color: #333;
-	text-align: center;
-	line-height: 60px;
-}
+  .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 30px;
+    background-color: #ffcc00;
+  }
 
-.el-footer {
-	background-color: #ffcc00;
-	color: #333;
-	text-align: center;
-	line-height: 60px;
-}
-.el-aside {
-	background-color: #EEAD0E	;
-	color: #333;
-	text-align: center;
-	line-height: 20px;
-}
+  .el-header {
+    background-color: #ffcc00;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
 
-.el-main {
-	background-color: #E9EEF3;
-	color: #333;
-	text-align: center;
-	/* line-height: 500px; */
-}
+  .el-footer {
+    background-color: #ffcc00;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
 
-body > .el-container {
-	margin-bottom: 20px;
-}
+  .el-aside {
+    background-color: #EEAD0E;
+    color: #333;
+    text-align: center;
+    line-height: 20px;
+  }
+
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    /* line-height: 500px; */
+  }
+
+  body > .el-container {
+    margin-bottom: 20px;
+  }
 
 
-  
 </style>
--- src/components/picture/Picture2.vue
+++ src/components/picture/Picture2.vue
@@ -1,33 +1,93 @@
 <template>
   <div>
-    <el-container>
-      <el-aside width="200px">Aside</el-aside>
-      <el-main>Main</el-main>
+    <el-container class="el-container-book">
+      <el-aside class="el-aside-book" style="width: 200px;">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo">
+
+          <el-submenu v-for="(menu,index) in menus" :key="index" :index="menu.menuId">
+            <template slot="title"><span>{{menu.menuName}}</span></template>
+            <el-menu-item v-for="(subMenu,index2) in menu.children" :key="index2" :index="subMenu.menuId" @click="consoleLog(subMenu.menuName)">
+              {{subMenu.menuName}}
+            </el-menu-item>
+          </el-submenu>
+
+          <!--<el-submenu index="1">-->
+          <!--<template slot="title"><span>导航一</span></template>-->
+          <!--<el-menu-item index="1">选项1-1</el-menu-item>-->
+          <!--<el-menu-item index="1">选项1-2</el-menu-item>-->
+          <!--</el-submenu>-->
+          <!--<el-submenu index="2">-->
+          <!--<template slot="title"><span>导航二</span></template>-->
+          <!--<el-menu-item index="4">选项1-3</el-menu-item>-->
+          <!--</el-submenu>-->
+        </el-menu>
+
+      </el-aside>
+      <el-main class="el-main-book">Main</el-main>
     </el-container>
 
   </div>
 </template>
 
 <script>
+  export default {
+    data() {
+      return {
+        menus: [
+          {
+            menuId: "1",
+            menuName: "导航一",
+            children: [
+              {
+                menuId: "2",
+                menuName: "导航1-1",
+              },
+              {
+                menuId: "3",
+                menuName: "导航1-2",
+              }
+            ]
+          },
+          {
+            menuId: "4",
+            menuName: "导航2",
+            children: [
+              {
+                menuId: "5",
+                menuName: "导航2-1",
+              }
+            ]
+          }
+        ]
+      }
+    },
+    methods: {
+      consoleLog(val){
+        console.log("AAA:"+val);
+      }
+    }
+  }
 
 </script>
 
-<style>
-  .el-aside {
-    background-color: #ffcc00	;
+<style scope>
+  .el-aside-book {
+    background-color: #ffcc00;
     color: #333;
     text-align: center;
     line-height: 20px;
   }
 
-  .el-main {
+  .el-main-book {
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
-     line-height: 500px;
+    line-height: 500px;
   }
 
-  body > .el-container {
+  .el-container-book {
     margin-bottom: 20px;
   }
 
