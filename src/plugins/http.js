e6532bc08c26f578ab6d13b460a6d2f953b53d36
1916931393@qq.com <1916931393@qq.com>
2019-12-23 06:57:17 +0800

    http拦截器

--- src/plugins/http.js
+++ src/plugins/http.js
@@ -2,63 +2,60 @@ import axios from 'axios';
 
 import context from '../main.js'
 
-axios.defaults.timeout = 1000 * 60 ;
+
+axios.defaults.timeout = 1000 * 60;
 // axios.defaults.baseURL = 'http://192.144.179.13:6060';
 axios.defaults.baseURL = 'https://www.joezzz.cn/hd';
 // axios.defaults.baseURL = 'http://127.0.0.1:6060';
 
 //http request 拦截器
 axios.interceptors.request.use(
-  config => {
-    config.data = JSON.stringify(config.data);
-    config.headers = {
-      'Content-Type': 'application/json'
+    config => {
+        config.data = JSON.stringify(config.data);
+        config.headers = {
+            'Content-Type': 'application/json'
+        }
+        return config;
+    },
+    error => {
+        return Promise.reject(err);
     }
-    return config;
-  },
-  error => {
-    return Promise.reject(err);
-  }
 );
 
 
 //http response 拦截器
 axios.interceptors.response.use(
-  response => {
-    if (response.data.code == 0) {
-      context.$alert(response.data.message, '提示', {
-        showClose: false,//不显示右上角关闭符号
-      })
-    } else if (response.data.code == 3) {
-      context.$alert(response.data.message, '提示', {
-        showClose: false,//不显示右上角关闭符号
-      }).then(()=>{
-        window.location.href = '/login';
-      });
-      // router.push({
-      //   path: "/login",
-      //   querry: {redirect: router.currentRoute.fullPath}//从哪个页面跳转
-      // })
+    response => {
+        if (response.data.code == 0) { //0=请求失败
+            context.$alert(response.data.message, '提示', {
+                showClose: false,//不显示右上角关闭符号
+            })
+        } else if (response.data.code == 3) { //3=验签失效，需重新登录
+            context.$alert(response.data.message, '提示', {
+                showClose: false,//不显示右上角关闭符号
+            }).then(() => {
+                context.$router.push({path: '/login'});
+            });
+        }
+        return response;
+    },
+    error => {
+        return Promise.reject(error)
     }
-    return response;
-  },
-  error => {
-    return Promise.reject(error)
-  }
 )
 
 export function post(url, data = {}) {
-  return new Promise((resolve, reject) => {
-    const param = {};
-    param.body = data;
-    param.timeStr = new Date();
-    param.signature = localStorage.getItem("signature");
-    param.userId = localStorage.getItem("userId");
-    axios.post(url, param)
-      .then(response => {
-        resolve(response.data);
-      }, err => {
-        reject(err)
-      })
-  })
+    return new Promise((resolve, reject) => {
+        const param = {};
+        param.body = data;
+        param.timeStr = new Date();
+        param.signature = localStorage.getItem("signature");
+        param.userId = localStorage.getItem("userId");
+        axios.post(url, param)
+            .then(response => {
+                resolve(response.data);
+            }, err => {
+                reject(err)
+            })
+    })
 }
